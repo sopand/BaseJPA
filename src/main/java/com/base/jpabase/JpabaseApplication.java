80dc28ead00f40d2d808a3b099f60a55a085c149
sopand <pows1031@naver.com>
2024-05-19 15:04:05 +0900

    first

---
+++ src/main/java/com/base/jpabase/JpabaseApplication.java
@@ -0,0 +1,13 @@
+package com.base.jpabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/base/jpabase/JpabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.jpabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
