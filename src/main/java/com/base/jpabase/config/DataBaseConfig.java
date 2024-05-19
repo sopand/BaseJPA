44b66f3eac829ae59446dba746253119c2830535
sopand <pows1031@naver.com>
2024-05-19 15:45:00 +0900

    TEST

---
+++ src/main/java/com/base/jpabase/config/DataBaseConfig.java
@@ -0,0 +1,27 @@
+package com.base.jpabase.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.yml")
+public class DataBaseConfig {
+	
+	@Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource(HikariConfig hikariConfig) {
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return dataSource;
+    }
+}
